6df8fb57322f518fa9b7a8619714ca8fd2bb0ade
Leonardo Diaz <jldiaz9623@gmail.com>
2025-07-25 10:24:18 -0500

    Manejo de conexion con variables de entorno

--- app/database.py
+++ app/database.py
@@ -1,8 +1,18 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin:Lupe241020@test-db.cuxyckg6s2ei.us-east-1.rds.amazonaws.com:3306/bibliotek"
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
